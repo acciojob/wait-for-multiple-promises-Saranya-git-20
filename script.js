71a55bfaa47466f660426085295af5d313c6bfca
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-30 21:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-//your JS code here. If required.
+const tableBody = document.getElementById('output');
+const promise=[
+	createPromise(1),
+	createPromise(2),
+    createPromise(3)
+]
+function createPromise(index){
+	const time = Math.floor(Math.random() * 3) + 1;
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve({ index: index, time: time });
+		},time * 1000)
+	})
+}
+
+Promise.all(promise).then((result)=>{
+	tableBody.innerHTML= ""; 
+	 let totalTime = 0;
+	result.forEach((result) =>{
+		totalTime += result.time;
+		const row = document.createElement('tr');
+		const namecell = document.createElement('td')
+		namecell.textContent = `Promise ${result.index}`;
+
+		const timecell = document.createElement('td')
+		timecell.textContent = result.time
+
+		row.appendChild(namecell);
+		row.appendChild(timecell);
+	    tableBody.appendChild(row);
+	})
+	 const totalRow = document.createElement('tr');
+	const total = document.createElement('td')
+	total.textContent = "Total";
+
+	const totalvalue = document.createElement('td')
+	totalvalue.textContent = totalTime
+
+	totalRow.appendChild(total);
+	totalRow.appendChild(totalvalue);
+	tableBody.appendChild(totalRow);
+})
